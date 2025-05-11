310c6247fca6606a584ef2e62b9ee30eeb990d8f
Nareshcybersecurity <naresh.cybersecofficial@gmail.com>
2025-05-11 20:29:45 +0530

    updated NavBar

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -12,20 +12,20 @@ const Navbar = () => {
           className="h-auto w-auto flex flex-row items-center"
         >
 
-          <span className="font-bold ml-[10px] hidden md:block text-gray-300">
+          <span className=" font-sans text-lg font-bold ml-[10px] hidden md:block text-gray-300">
             Naresh Ramalingam
           </span>
         </a>
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-me" className="cursor-pointer">
+            <a href="#about-me" className="cursor-pointer font-bold">
               About me
             </a>
-            <a href="#skills" className="cursor-pointer">
+            <a href="#skills" className="cursor-pointer font-bold">
               Skills
             </a>
-            <a href="#projects" className="cursor-pointer">
+            <a href="#projects" className="cursor-pointer font-bold">
               Projects
             </a>
           </div>
@@ -44,7 +44,7 @@ const Navbar = () => {
         alt={social.name}
         width={24}
         height={24}
-        className="cursor-pointer"
+        className="cursor-pointer w-7 h-7  min-w-[25px] transition-transform hover:scale-125 hover:backdrop-blur rounded-full "
       />
     </a>
           ))}
